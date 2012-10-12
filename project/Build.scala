409f0be2a1778c87a99faf046b614f01660ed454
Bing Ran <bing.ran@gmail.com>
2012-10-12 13:25:32 +0800

    moved to version 0.4

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "japid42"
-    val appVersion      = "0.3"
+    val appVersion      = "0.4"
 
     val appDependencies = Seq(
     		"org.apache.commons" % "commons-email" % "1.2"
