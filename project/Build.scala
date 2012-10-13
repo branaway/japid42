4b225a7297fadc2cd23bc08f9ace10c77c87eb43
Yuvi Masory <ymasory@gmail.com>
2012-10-12 22:14:02 -0400

    add commons-lang dependency, closes #2, remove literal tabs, format to scala convention

--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,17 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "japid42"
-    val appVersion      = "0.4"
+  val appName         = "japid42"
+  val appVersion      = "0.4"
 
-    val appDependencies = Seq(
-    		"org.apache.commons" % "commons-email" % "1.2"
-   // 		"javax.activation" % "activation" % "1.1",
-    //		"javax.mail" % "mail" % "1.4.1"
-    )
+  val appDependencies = Seq(
+    "org.apache.commons" % "commons-email" % "1.2",
+    "commons-lang" % "commons-lang" % "2.6"
+    // "javax.activation" % "activation" % "1.1",
+    //"javax.mail" % "mail" % "1.4.1"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
-    )
-
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+    // Add your own project settings here
+  )
 }
